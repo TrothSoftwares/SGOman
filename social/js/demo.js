dc1cffb994a7ce9fe79801ee412f14e93f179b5c
anvrnazr <anvrnazr@gmail.com>
2016-09-03 15:14:45 +0530

    before pushing to troth

--- social/js/demo.js
+++ social/js/demo.js
@@ -26,10 +26,12 @@ WebFontConfig = {
 $.contactButtons({
   effect  : 'slide-on-scroll',
   buttons : {
-    'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/SolarGarduae', extras: 'target="_blank"' },
-    'twitter':    { class: 'twitter',use:true,link: 'https://twitter.com/SolarandSafety', extras: 'target="_blank"' },
-    'youtube':    { class: 'youtube',      use: true, link: 'https://www.youtube.com/channel/UC6Z0hiDQqg7jALvTPRPdy9g', icon: 'youtube', extras: 'target="_blank"', title: 'Visit YouTube Channel'  },
-    'googleplus': { class: 'gplus',      use: true, link: 'https://www.youtube.com/channel/UC6Z0hiDQqg7jALvTPRPdy9g', icon: 'google-plus', extras: 'target="_blank"' , title: 'Visit on Google Plus'  },
+    'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/SolarGard-Oman-991541297578607', extras: 'target="_blank"' },
+    'twitter':    { class: 'twitter',use:true,link: 'https://twitter.com/solargard_oman', extras: 'target="_blank"' },
+    'youtube':    { class: 'youtube',      use: false, link: 'https://www.youtube.com/channel/UC6Z0hiDQqg7jALvTPRPdy9g', icon: 'youtube', extras: 'target="_blank"', title: 'Visit YouTube Channel'  },
+    'googleplus': { class: 'gplus',      use: true, link: 'https://plus.google.com/108529877498298749354', icon: 'google-plus', extras: 'target="_blank"' , title: 'Visit on Google Plus'  },
+    'instagram': { class: 'instagram',      use: true, link: 'https://www.instagram.com/solargard_oman', icon: 'instagram', extras: 'target="_blank"' , title: 'Follow on Instagram'  },
+
 
     'phone':      { class: 'phone separated',    use: true, link: '+96898311010',  title:'+96898311010' },
     'email':      { class: 'email',    use: true, link: 'sales@solargardoman.com' }
